f18f3551863b5e5425373070e76a888a91aa0dee
“llRosell” <llovein0920@gmail.com>
2025-02-04 14:47:08 +0900

    feat : 쿠폰ㄱRepository 낙관적락, 비관적락 적용

--- src/main/java/com/example/coupang/coupon/repository/CouponRepository.java
+++ src/main/java/com/example/coupang/coupon/repository/CouponRepository.java
@@ -1,11 +1,27 @@
 package com.example.coupang.coupon.repository;
 
 import com.example.coupang.coupon.entity.Coupon;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+    @Lock(LockModeType.OPTIMISTIC)
+    @Query("SELECT c FROM Coupon c WHERE c.user.id = :userId AND c.status = :status")
+    Coupon findByUserIdAndStatusWithOptimistic(Long userId, String status);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT c FROM Coupon c WHERE c.status = :status")
+    List<Coupon> findCouponsByStatusWithPessimisticLock(@Param("status") String status);
+
+
+    long countCouponsByStatus(String status);
+
     // 유저 ID를 기준으로 모든 쿠폰 조회
     List<Coupon> findAllByUserId(Long userId);
 }
