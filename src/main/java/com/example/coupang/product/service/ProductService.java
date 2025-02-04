cbeaf95c289422638bdde33efc040a9d7bb8a9e0
DeaHyun0911 <eogus0911@gmail.com>
2025-02-04 21:37:08 +0900

    refactor: pageable -> controller
    - pageable 객체로 controller 에서 받아오도록 변경

--- src/main/java/com/example/coupang/product/service/ProductService.java
+++ src/main/java/com/example/coupang/product/service/ProductService.java
@@ -2,13 +2,14 @@ package com.example.coupang.product.service;
 
 import com.example.coupang.common.RestPage;
 import com.example.coupang.product.dto.response.ProductResponseDto;
+import org.springframework.data.domain.Pageable;
 
 public interface ProductService {
 
     ProductResponseDto getProductById(Long id);
 
-    RestPage<ProductResponseDto> getProductsV1(Integer page, Integer size, String title);
+    RestPage<ProductResponseDto> getProductsV1(Pageable pageable, String title);
 
-    RestPage<ProductResponseDto> getProductsV2(Integer page, Integer size, String title);
+    RestPage<ProductResponseDto> getProductsV2(Pageable pageable, String title);
 
 }
